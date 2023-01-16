dc760a62c8bcfb1fc0975b76a44b6e481320e23b
BoyoungYun <index_1234@naver.com>
2023-01-17 02:46:06 +0900

    Add sidoList.js & data binding in main.js

--- src/main.js
+++ src/main.js
@@ -1,21 +1,44 @@
-import React from "react";
+import {React, useState} from "react";
 import axios from "axios";
+import sidoList from "./sidoList.js";
 function Main()
 {
     const SERVICE_KEY = process.env.REACT_APP_SERVICE_KEY;
+    const [sidoCode, setSidoCode] = useState('');
+    const [sigunCode, setSigunCode] = useState('');
+    const [sidoFlag, setSidoFlag] = useState(false);
+    const [sidoIdx, setIdx] = useState(0);
     return (
-        <div onClick={()=>dataLoad()}>
-            데이터 표시 중...
-        </div>
+        <>
+            <select onChange={(e)=>{setIdx(e.target.value); setSidoFlag(true)}}>
+            {
+                sidoList.map((a,i)=>
+                    <option value={sidoList[i].id}>{sidoList[i].name}</option>
+                )
+            }
+            </select>
+            {
+                sidoFlag===true
+                ? <select onChange={(e)=>{setSidoCode(sidoList[sidoIdx].code); setSigunCode(e.target.value);}}>
+                    {
+                        sidoList[sidoIdx].detail.map((a,i)=>
+                        <option value={sidoList[sidoIdx].detail_code[i]}>{sidoList[sidoIdx].detail[i]}</option>
+                        )
+                    }
+                </select>
+                : null
+            }
+            <div onClick={()=>{dataLoad()}}>보호소 조회</div>
+        </>
     );
 
     async function dataLoad()
     {
-        const url = `https://apis.data.go.kr/1543061/abandonmentPublicSrvc/sido?serviceKey=${SERVICE_KEY}`;
+        const url = `http://apis.data.go.kr/1543061/abandonmentPublicSrvc/shelter?serviceKey=${SERVICE_KEY}`;
         const response = await axios.get(url, {
             params: {
-                numOfRows: 3,
-                pageNo: 1,
+                upr_cd: sidoCode,
+                org_cd: sigunCode,
                 _type: "json"
             }
         });
